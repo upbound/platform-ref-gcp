ef3ddf88a21ad08b338a2aec6626144ea4bf3aac
Yury Tsarev <yury@upbound.io>
2022-10-25 23:21:34 +0200

    Delete user.sql in test hook to avoid the orphan state
    Signed-off-by: Yury Tsarev <yury@upbound.io>

--- examples/testhooks/delete-release.sh
+++ examples/testhooks/delete-release.sh
@@ -2,5 +2,6 @@
 set -aeuo pipefail
 
 # Delete the release before deleting the cluster not to orphan the release object
+# Delete user.sql before deleteing the database instance for the similar reason
 # Note(turkenh): This is a workaround for the infamous dependency problem during deletion.
-${KUBECTL} delete release --all
+${KUBECTL} delete release.helm.crossplane.io,user.sql.gcp.upbound.io --all
