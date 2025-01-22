b80372eee14cb9a3ff103d0e0e9768aaeb133a14
Tobias Kässer <tobias.kasser@upbound.io>
2025-01-22 20:17:47 +0100

    Add project.mk

---
+++ project.mk
@@ -0,0 +1,5 @@
+PROJECT_NAME := platform-ref-gcp
+UPTEST_INPUT_MANIFESTS := examples/cluster-claim.yaml,examples/postgres-claim.yaml,examples/mysql-claim.yaml,examples/app-claim.yaml
+UPTEST_SKIP_IMPORT := true
+UPTEST_SKIP_UPDATE := true
+UPTEST_DEFAULT_TIMEOUT = 3600s
